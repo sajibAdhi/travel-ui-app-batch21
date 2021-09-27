e80c848c25a98916abfc310a39ffa50cda7d93ce
sajibadhi <tosajibadhi@gmail.com>
2021-09-27 11:51:38 +0600

    Widgets

---
+++ lib/widgets/travel_blog.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:travel_app_batch21/models/travel.dart';
+
+class TravelBlog extends StatelessWidget {
+  final _list = Travel.generateTravelBlog();
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView.builder(
+      physics: BouncingScrollPhysics(),
+      itemCount: _list.length,
+      itemBuilder: (context, index) {
+        var travel = _list[index];
+        return Stack(
+          children: [
+            Container(
+              padding: EdgeInsets.all(5),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(5),
+                child: Image.asset(
+                  travel.url,
+                  fit: BoxFit.cover,
+                  width: MediaQuery.of(context).size.width,
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
