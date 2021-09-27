03fb3e46ca4a343198c899525f3734e5a7444cb0
sajibadhi <tosajibadhi@gmail.com>
2021-09-27 11:51:16 +0600

    modals

--- lib/models/travel.dart
+++ lib/models/travel.dart
@@ -4,9 +4,11 @@ class Travel {
   String url;
   Travel(this.name, this.location, this.url);
 
-  List<Travel> generateTravelBlog() {
+  static List<Travel> generateTravelBlog() {
     return [
-      Travel('Place1', "place1", url),
+      Travel('Place1', "place1", 'images/img1.png'),
+      Travel('Place2', "place2", 'images/img2.png'),
+      Travel('Place3', "place3", 'images/img3.png'),
     ];
   }
 }
